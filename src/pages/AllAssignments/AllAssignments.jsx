5f4eaa13e0959b5cae59117bbc872d12d3a608ab
nahidHassan405 <infosharemail405@gmail.com>
2024-10-29 19:55:38 +0600

    all assignment care added

--- src/pages/AllAssignments/AllAssignments.jsx
+++ src/pages/AllAssignments/AllAssignments.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AssignmentCard from "./AssignmentCard";
 
 const AllAssignments = () => {
-  return <div></div>;
+  const [assignments, setAssignments] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios("fake.json");
+        setAssignments(result.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  });
+  return (
+    <div>
+      <h2>All Assignments come here</h2>
+      <div className="grid grid-cols-4 gap-6 max-w-7xl mx-auto mt-12">
+        {assignments.map((assignment) => (
+          <AssignmentCard key={assignment.marks}></AssignmentCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllAssignments;
