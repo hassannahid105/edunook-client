801d8a87b612552b319b8f26618976fd4f298768
nahidHassan405 <infosharemail405@gmail.com>
2024-11-21 20:39:01 +0600

    card design

--- src/pages/AllAssignments/AssignmentCard.jsx
+++ src/pages/AllAssignments/AssignmentCard.jsx
@@ -17,32 +17,32 @@ const AssignmentCard = ({ assignment, handleDelete, handleUpdate }) => {
   const { user } = useAuth();
   return (
     <div className="shadow-lg relative bg-gray-100">
-      <figure>
+      <figure className="h-[250px]">
         <img src={thumbnail} alt="" />
       </figure>
       <div className="">
-        <h2 className="text-2xl font-extrabold mx-6 pt-4">
+        <h2 className="text-2xl font-extrabold mx-6 pt-8">
           {title}
           <div className="absolute top-6 text-lg font-normal capitalize bg-main  text-white px-4 py-1">
             {difficulty}
           </div>
         </h2>
         <p className="text-sm  mx-4 py-1 pl-2 pb-4">
-          {description.substring(0, 50)}
+          {description.substring(0, 50)}...
         </p>
         <div className="flex  gap-10 mx-6 uppercase">
-          <p className="flex flex-col justify-center text-center bg-[#e8ede6] w-4/12 py-2 text-lg font-bold gap-1">
-            <span className="text-2xl">{marks}</span>
+          <p className="flex flex-col justify-center text-center bg-[#e8ede6] w-4/12  text-lg font-bold gap-1">
+            Marks: {marks}
           </p>
-          <div className="flex flex-col gap-4 mt-4  ">
+          <div className="flex flex-col gap-4 ">
             <button
               onClick={() => handleDelete(user?.email, cardUser, _id)}
-              className="flex justify-start items-center  text-4xl"
+              className="flex justify-start items-center  text-xl"
             >
               <RiDeleteBin7Fill />
               <span className="text-xl">Delete</span>
             </button>
-            <Link to={`/update/${_id}`} className="flex text-4xl">
+            <Link to={`/update/${_id}`} className="flex text-xl">
               <GrDocumentUpdate />
               <span className="text-xl">Update</span>
             </Link>
